770c75c81d7005cb0cd3352be6bb92dbe0c05802
Dns911 <vasilevskydenis@gmail.com>
2023-02-23 11:51:17 +0300

    23.02

--- src/main/java/com/epam/buber/controller/info/AttrValue.java
+++ src/main/java/com/epam/buber/controller/info/AttrValue.java
@@ -5,6 +5,11 @@ public class AttrValue {
     }
     public static final String GUEST_MSG = "Гость";
     public static final String STATUS_ERR_MSG = "У вас не активный статус,\nсвяжитесь с администратором.";
+    public static final String CAR_ID_ERR_MSG_1 = "Автомодиль не найден";
+    public static final String CAR_ID_ERR_MSG_2 = "Введен некорректный номер,\nв формате 1234AB-7";
+
+    public static final String DRIVER_OUT = "Успешно завершено";
+    public static final String DRIVER_OUT_ERR = "Завершите полученный заказ";
 
 
 
